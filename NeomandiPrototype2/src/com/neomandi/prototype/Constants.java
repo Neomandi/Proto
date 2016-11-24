ddc6e84b7b972223ce1d612eaf5c9935cd465681
varunnm <varun@neomandi.com>
2016-11-24 11:24:26 +0530

    commit

---
+++ NeomandiPrototype2/src/com/neomandi/prototype/Constants.java
@@ -0,0 +1,10 @@
+package com.neomandi.prototype;
+
+public interface Constants 
+{
+	//Define database attributes.
+    public static final String DBNAME = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/neomandi";
+    public static final String UID = "root";
+    public static final String PWD = "j2ee";
+}
