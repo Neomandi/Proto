e1d8761b646335022233e560c99a1f602917c5c1
Manjunath-Setty <manjunath@neomandi.com>
2017-05-04 12:16:21 +0530

    Signed-off-by: Manjunath-Setty <manjunath@neomandi.com>

--- NeomandiPrototype/src/com/neomandi/prototype/Constants.java
+++ NeomandiPrototype/src/com/neomandi/prototype/Constants.java
@@ -2,9 +2,7 @@ package com.neomandi.prototype;
 public interface Constants 
 {
     public static final String DBNAME = "com.mysql.jdbc.Driver";
-
     public static final String URL = "jdbc:mysql://localhost:3306/neomandi";
-
     public static final String UID = "root";
     public static final String PWD = "j2ee";
 }
